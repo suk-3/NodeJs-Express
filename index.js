ddb0fd4ffad83c158eff97940b66afade9885130
Suhas U Kekuda <suhas.ukekuda@lilly.com>
2024-08-04 23:08:25 +0530

    Added security middleware

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const helmet = require("helmet")
 const config = require('./config');
+
 require('dotenv').config();
 
 // Middleware
 app.use(express.json()); // Parse JSON bodies
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
+app.use(helmet()); // Helmet middleware for security
 
 app.disable('x-powered-by'); // Diable x-powered-by headers for security reasons
 app.set('etag', 'strong'); // changing etag from default weak to strong
